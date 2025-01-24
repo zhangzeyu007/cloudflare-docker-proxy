993e81daf82d4b81b50427ba9069cf1bf3f485db
Jobs-Devin <30886537+zhangzeyu007@users.noreply.github.com>
2025-01-24 18:51:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.zeyu.link" + CUSTOM_DOMAIN]: dockerHub,
+  ["quay.zeyu.link" + CUSTOM_DOMAIN]: "https://quay.io",
+  ["gcr.zeyu.link" + CUSTOM_DOMAIN]: "https://gcr.io",
+  ["k8s-gcr.zeyu.link" + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  ["k8s.zeyu.link" + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  ["ghcr.zeyu.link" + CUSTOM_DOMAIN]: "https://ghcr.io",
+  ["cloudsmith.zeyu.link" + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  ["ecr.zeyu.link" + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.zeyu.link" + CUSTOM_DOMAIN]: dockerHub,
 };
 
 function routeByHosts(host) {
